737df82076ad606b63c528abd12785bf9658adf3
Kirill Aristov <kirillaristov14@yandex.ru>
2026-02-18 16:17:55 +0300

    fix bag

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -1,10 +1,16 @@
 from pathlib import Path
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-at9n+l3^##ud(eaw5298yp_pvvfg-e^r67up5rh^phbgo0w3b$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
